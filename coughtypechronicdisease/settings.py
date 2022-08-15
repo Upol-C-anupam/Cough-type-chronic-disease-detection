73a6c8515d00dde6972978701c929fd45b3251ab
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2022-08-16 00:39:04 +0600

    1st commit

--- coughtypechronicdisease/settings.py
+++ coughtypechronicdisease/settings.py
@@ -80,6 +80,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
